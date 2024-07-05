885fbbf3e183aa9c3d007b139b8f50393449c6f3
alihusnainsaeedantechpvt <alihusnain@saeedantechpvt.com>
2024-07-05 16:56:20 +0500

    change

--- app/Enums/TradeOrderTypeEnum.php
+++ app/Enums/TradeOrderTypeEnum.php
@@ -5,12 +5,19 @@ namespace App\Enums;
 final class TradeOrderTypeEnum
 {
     const BUY = 'buy';
+
     const SELL = 'sell';
+
     const SELL_LIMIT = 'Sell Limit'; // Corrected constant declaration
+
     const BUY_LIMIT = 'Buy Limit'; // Corrected constant declaration
+
     const BUY_STOP = 'Buy Stop';
+
     const SELL_STOP = 'Sell Stop'; // Corrected constant name
+
     const BUY_SELL_LIMIT = 'Buy Sell Limit';
+
     const SELL_STOP_LIMIT = 'Sell Stop Limit'; // Corrected missing closing quote
 
     // Method to retrieve all available trade order types
